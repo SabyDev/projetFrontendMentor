43f6ada7ccdfbf2c38678f3e037d1725485700bd
sabrina michee <martmichee@gmail.com>
2022-05-14 14:21:10 +0200

    correction html, et nouvel essai pour ouvrir et fermer la div response

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,16 +1,14 @@
 // J'appelle les elements dont j'ai besoin
-const questions = document.querySelectorAll('.question');
-const btnOk = document.querySelectorAll('.ok');
-console.log(btnOk);
+const questions = document.querySelector('.question');
+const btnOk = document.querySelector('.ok');
+const response1 = document.querySelector('.response'); 
+const questionBox= document.querySelector('.questionBox') 
+console.log(response1);
 let divText= document.createElement("div");
-// rattachement à un parent body
-questions.forEach((e) =>{
-    e.appendChild(divText)});
 
-btnOk.forEach((e) => {
-    e.addEventListener('click',() =>{
-        // création d'une div 
-divText.TextNodes=(' You can invite up to 2 additional users on the Free plan. There is no limit on team members for the Premium plan.')
 
-    })
+btnOk.addEventListener('click', function(){
+ response1.style.display = 'flex';
+ questionBox.style.fontWeight = 'bolder'  
+ console.log('cc');
 })
